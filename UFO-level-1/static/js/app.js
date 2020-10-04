126d2f63311ea736bf9d74ae086e2ce3740507ba
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-03 20:22:45 -0700

    set only one filter criteria

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,8 +15,6 @@ button.on("click", runEnter);
 form.on("submit", runEnter);
 
 
-
-
 //Load data by default
 tableData.forEach((tableData) => {
     var row = tbody.append("tr");
@@ -40,68 +38,24 @@ function runEnter() {
 
     // Select the input element and get the raw HTML node
     var inputDate = d3.select("#datetime");
-    var inputCity = d3.select("#city");
-    var inputState = d3.select("#state");
-    var inputCountry = d3.select("#country");
-    var inputShape = d3.select("#shape");
+
 
     // Get the value property of the input element or use wildcard if blank
     var inputValueDate = inputDate.property('value');
-    var inputValueCity = inputCity.property("value");
-    var inputValueState = inputState.property("value");
-    var inputValueCountry = inputCountry.property("value");
-    var inputValueShape = inputShape.property("value");
-
-    // var inputValueDate = inputDate.property("value");
-    // var inputValueDate = inputValueDate.replace("","1/1/2010")
-
-    // if (inputCity.length > 0) {
-    //     var inputValueCity = inputCity.property("value");
-    // } else {
-    //     var inputValueCity = wildcard;
-    // }
-
-    // // var inputValueCity = inputCity.property("value");
-    // // var inputValueCity = inputValueCity.replace("","el cajon") 
-
-    // if (inputState === '') {
-    //     var inputValueState = wildcard;
-    // } else {
-    //     var inputValueState = inputState.property("value");
-    // }
-
-    // if (inputValueCountry === '') {
-    //     var inputValueCountry = wildcard;
-    // } else {
-    //     var inputValueCountry = inputCountry.property("value");
-    // }
-
-    // if (inputValueShape === '') {
-    //     var inputValueShape = wildcard;
-    // } else {
-    //     var inputValueShape = inputShape.property("value");
-    // }
 
 
     // Log event for error handling
     console.log(inputValueDate);
-    console.log(inputValueCity);
-    console.log(inputValueState);
-    console.log(inputValueCountry);
-    console.log(inputValueShape);
 
     // filter data based on elements
-    var filteredData = tableData.filter(Element => (Element.datetime === inputValueDate) || (Element.city === inputValueCity) || (Element.state === inputValueState) || (Element.country === inputValueCountry) || (Element.shape === inputValueShape));
 
-    // var filteredData = tableData.filter(Element => (Element.datetime === inputValueDate)); //Filter just on one element
-
-    // || (Element.city === inputValueCity) || (Element.city === inputValueState) || (Element.city === inputValueCountry) || (Element.state === inputValueShape)); //filter on all elements
+    var filteredData = tableData.filter(Element => (Element.datetime === inputValueDate)); //Filter just on one element
 
     // Log event for error handling
     console.log(filteredData);
 
     // If there is no type selected, return all objects:
-    if (inputValueDate === '' && inputValueCity === '' && inputValueState === '' && inputValueCountry === '' && inputValueShape === '') {
+    if (inputValueDate === '') {
         tableData.forEach((tableData) => {
             var row = tbody.append("tr");
             Object.entries(tableData).forEach(([key, value]) => {
